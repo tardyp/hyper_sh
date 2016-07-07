041d9f7f77fcdeecac7df88d9f7d17dddb32a80e
Pierre Tardy <pierre.tardy@intel.com>
2016-07-07 13:00:06 +0200

    Revert "use original aws4auth"
    This reverts commit e4244a01330af4076fa78133495cbaa85182855a.

--- hypercompose/api.py
+++ hypercompose/api.py
@@ -4,19 +4,7 @@ import urlparse
 
 from docker import Client
 
-from requests_aws4auth import AWS4Auth
-
-AWS4Auth.default_include_headers = ['host', 'content-type', 'date', 'x-hyper-*']
-AWS4Auth.SECURITY_TOKEN_HEADER = 'x-hyper-security-token'
-AWS4Auth.DATE_HEADER = 'x-hyper-date'
-AWS4Auth.CONTENT_SHA256_HEADER = 'x-hyper-content-sha256'
-AWS4Auth.CLIENT_CONTEXT_HEADER = 'x-hyper-client-context'
-AWS4Auth.HEADER_PREFIX = 'x-hyper-'
-AWS4Auth.SCOPE_SUFFIX = 'hyper_request'
-AWS4Auth.KEY_PREFIX = 'HYPER'
-AWS4Auth.AUTH_PREFIX = 'HYPER-HMAC-SHA256'
-old_cano_path = AWS4Auth.amz_cano_path
-AWS4Auth.amz_cano_path = lambda self, path: old_cano_path(self, path[1:])
+from .requests_aws4auth import AWS4Auth
 
 
 class Hyper(Client):
@@ -32,7 +20,6 @@ class Hyper(Client):
         self.auth = AWS4Auth(self.creds['accesskey'], self.creds['secretkey'],
                              url.netloc.split(".")[0], 'hyper')
         self._version = "1.23"
-        self._auth_configs['HttpHeaders'] = {'content-type': 'application/json'}
 
     def create_network(*arg, **kw):
         #ignore network creation
